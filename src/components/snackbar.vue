bddcd836663e7d6343be75cd62d6e5abb8919249
m-javad7 <m.javad7721@gmail.com>
2024-05-08 21:41:15 +0330

    .

---
+++ src/components/snackbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-snackbar
+      :model-value="modelValue"
+      @click:outside="hideSnackbar"
+      @update:model-value="updateModelValue"
+      :color="color"
+      :timeout="timeout"
+    >
+      {{ message }}
+    </v-snackbar>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      modelValue: {
+        type: Boolean,
+        required: true,
+      },
+      color: String,
+      message: String,
+      timeout: {
+        type: Number,
+        default: 3000,
+      },
+    },
+    methods: {
+      hideSnackbar() {
+        this.$emit("update:modelValue", false);
+      },
+      updateModelValue(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  };
+  </script>
+  
--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,71 +1,181 @@
 <template>
-    <v-card
-      class="mx-auto"
-      width="300"
-    >
-      <v-list v-model:opened="open">
-        <v-list-item prepend-icon="mdi-home" title="Home"></v-list-item>
-  
-        <v-list-group value="Users">
-          <template v-slot:activator="{ props }">
-            <v-list-item
-              v-bind="props"
-              prepend-icon="mdi-account-circle"
-              title="Users"
-            ></v-list-item>
-          </template>
-  
-          <v-list-group value="Admin">
-            <template v-slot:activator="{ props }">
-              <v-list-item
-                v-bind="props"
-                title="Admin"
-              ></v-list-item>
-            </template>
-  
-            <v-list-item
-              v-for="([title, icon], i) in admins"
-              :key="i"
-              :prepend-icon="icon"
-              :title="title"
-              :value="title"
-            ></v-list-item>
-          </v-list-group>
-  
-          <v-list-group value="Actions">
-            <template v-slot:activator="{ props }">
-              <v-list-item
-                v-bind="props"
-                title="Actions"
-              ></v-list-item>
-            </template>
-  
-            <v-list-item
-              v-for="([title, icon], i) in cruds"
-              :key="i"
-              :prepend-icon="icon"
-              :title="title"
-              :value="title"
-            ></v-list-item>
-          </v-list-group>
-        </v-list-group>
-      </v-list>
+  <v-container>
+    <v-toolbar flat>
+      <v-toolbar-title>لیست موقعیت‌ها</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-snackbar v-model="deleteSnackbar.show" :color="deleteSnackbar.color" :timeout="10000" top>
+      {{ deleteSnackbar.message }}
+      <v-btn color="green" @click="confirmDelete">بله</v-btn>
+      <v-btn color="white" @click="cancelDelete">خیر</v-btn>
+    </v-snackbar>
+
+    <v-card>
+      <v-infinite-scroll :height="500" :items="locations" :onLoad="loadMoreItems">
+        <v-table>
+          <thead>
+            <tr>
+              <th>نام</th>
+              <th>شرح</th>
+              <th>عملیات</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in locations" :key="item.id" :class="{ 'edit-mode': isEditingRow(item) }">
+              <td>
+                <v-text-field v-if="isEditingRow(item)" v-model="item.name" density="compact" />
+                <span v-else>{{ item.name }}</span>
+              </td>
+              <td>
+                <v-text-field v-if="isEditingRow(item)" v-model="item.description" density="compact" />
+                <span v-else>{{ item.description }}</span>
+              </td>
+              <td>
+                <v-icon small @click="toggleEdit(item)" :color="isEditingRow(item) ? 'green' : 'primary'">
+                  mdi-pencil
+                  {{ isEditingRow(item) ? 'ثبت' : 'ویرایش' }}
+                </v-icon>
+                <v-icon small @click="requestDelete(item)" class="red--text">
+                  mdi-delete
+                </v-icon>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-infinite-scroll>
+      <v-snackbar v-model="snackbar.show" :color="snackbar.color" :timeout="3000">
+        {{ snackbar.message }}
+      </v-snackbar>
     </v-card>
-  </template>
-  <script>
-    export default {
-      data: () => ({
-        open: ['Users'],
-        admins: [
-          ['Management', 'mdi-account-multiple-outline'],
-          ['Settings', 'mdi-cog-outline'],
-        ],
-        cruds: [
-          ['Create', 'mdi-plus-outline'],
-          ['Read', 'mdi-file-outline'],
-          ['Update', 'mdi-update'],
-          ['Delete', 'mdi-delete'],
-        ],
-      }),
-    }
-  </script>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      locations: [],
+      editRow: null,
+      snackbar: {
+        show: false,
+        color: '',
+        message: '',
+      },
+      deleteSnackbar: {
+        show: false,
+        color: '',
+        message: '',
+      },
+      limit: 5,
+    };
+  },
+
+  mounted() {
+    this.fetchLocations();
+  },
+
+  methods: {
+    fetchLocations() {
+      this.$http.get('/personnel')
+        .then((response) => {
+          this.locations = response.data;
+        })
+        .catch((error) => {
+          this.showSnackbar('خطا در دریافت داده‌ها', 'red');
+          console.error('خطا در دریافت داده‌ها:', error);
+        });
+    },
+
+    isEditingRow(item) {
+      return this.editRow === item; // بررسی اینکه آیا ردیف در حالت ویرایش است
+    },
+
+    toggleEdit(item) {
+      if (this.isEditingRow(item)) {
+        this.save(item); // اگر در حالت ویرایش است، ذخیره می‌شود
+        this.editRow = null; // خروج از حالت ویرایش
+      } else {
+        this.editRow = item; // وارد حالت ویرایش شوید
+      }
+    },
+
+    save(item) {
+      const url = `/personnel/${item.id}`;
+      this.$http.put(url, item)
+        .then((response) => {
+          const index = this.locations.findIndex((e) => e.id === item.id);
+          if (index !== -1) {
+            Object.assign(this.locations[index], response.data.data);
+          }
+          this.showSnackbar('تغییرات با موفقیت ذخیره شد', 'green');
+        })
+        .catch((error) => {
+          this.showSnackbar('خطا در ذخیره‌سازی', 'red');
+          console.error('خطا در ذخیره‌سازی:', error);
+        });
+    },
+
+    requestDelete(item) {
+      this.itemToDelete = item;
+      this.deleteSnackbar = {
+        show: true,
+        color: 'red',
+        message: 'آیا از حذف این آیتم مطمئن هستید؟',
+      };
+    },
+
+    confirmDelete() {
+      if (!this.itemToDelete) return;
+
+      this.$http.delete(`/personnel/${this.itemToDelete.id}`)
+        .then(() => {
+          this.locations = this.locations.filter(loc => loc.id !== this.itemToDelete.id); // حذف آیتم
+          this.showSnackbar('آیتم با موفقیت حذف شد', 'green');
+          this.resetDeleteSnackbar();
+        })
+        .catch((error) => {
+          this.showSnackbar('خطا در حذف آیتم', 'red');
+          console.error('خطا در حذف:', error);
+          this.resetDeleteSnackbar();
+        });
+    },
+
+    cancelDelete() {
+      this.resetDeleteSnackbar();
+    },
+
+    resetDeleteSnackbar() {
+      this.deleteSnackbar.show = false;
+      this.itemToDelete = null;
+    },
+
+    showSnackbar(message, color) {
+      this.snackbar.show = true;
+      this.snackbar.color = color;
+      this.snackbar.message = message;
+    },
+
+    loadMoreItems({ done }) {
+      this.$http.get('/personnel', {
+        params: {
+          offset: this.locations.length,
+          limit: this.limit,
+        },
+      })
+        .then((response) => {
+          if (response.data.length > 0) {
+            this.locations.push(...response.data); // افزودن داده‌های جدید
+            done('ok'); // نشان‌دهنده موفقیت
+          } else {
+            done('done'); // نشان‌دهنده پایان داده‌ها
+          }
+        })
+        .catch((error) => {
+          this.showSnackbar('خطا در بارگذاری آیتم‌های بیشتر', 'red');
+          done('error');
+          console.error('خطا در بارگذاری آیتم‌های بیشتر:', error);
+        });
+    },
+  },
+};
+</script>
