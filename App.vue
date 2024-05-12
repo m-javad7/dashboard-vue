300a45435bab743fb8638cd93154696f4aa6fa6f
M.Javad Darezereshki <128983576+m-javad7@users.noreply.github.com>
2024-05-12 13:23:13 +0330

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+  <component :is="getLayoutComponent">
+      <router-view />
+  </component>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import DefaultLayout from '@/layouts/default.vue';
+import ProductLayout from '@/layouts/ProductLayout'
+import { useRoute } from 'vue-router';
+import Null from './layouts/Null.vue';
+
+const route = useRoute();
+
+const getLayoutComponent = computed(() => {
+  // استفاده از یک شرط برای تعیین Layout بر اساس مسیر
+  if (route.fullPath.startsWith('/Production')) {
+    return ProductLayout;
+  }
+  if (route.fullPath === '/login') {
+    // اگر مسیر فعلی مربوط به لاگین است، لایوتی را بر نگردان
+    return Null;
+  }
+  return DefaultLayout;
+});
+</script>
+
+
