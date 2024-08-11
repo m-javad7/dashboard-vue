1234efe1304d90e6d04a35f00a02f2253d89e8c5
m-javad7 <m.javad7721@gmail.com>
2024-08-11 22:14:49 +0330

    .

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,124 +1,77 @@
 import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import Fonts from 'unplugin-fonts/vite'
-import Layouts from 'vite-plugin-vue-layouts'
-import Vue from '@vitejs/plugin-vue'
-import VueRouter from 'unplugin-vue-router/vite'
-import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import Pages from 'vite-plugin-pages'
-import { VitePWA } from 'vite-plugin-pwa' // اضافه کردن پلاگین PWA
+import vue from '@vitejs/plugin-vue'
+import vuetify from '@vuetify/vite-plugin'
+import { VitePWA } from 'vite-plugin-pwa'
+
+import path from 'path'
+
+
+const VitePWAConfig = {
+  mode: "development",
+  base: "./",
+  srcDir: "src",
+  filename: "sw.ts",
+  includeAssets: ["/favicon.png"],
+  strategies: "injectManifest",
+  manifest: {
+    name: "GCP Testing App",
+    short_name: "GCP",
+    theme_color: "#0094f0",
+    start_url: "./#/home/welcome",
+    scope: "./",
+    display: "standalone",
+    background_color: "#0094f0",
+    icons: [
+      {
+        src: "./",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "public/4.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "./img/icons/maskable_icon_x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable"
+      },
+      {
+        src: "./img/icons/maskable_icon_x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  },
+};
+
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
-    VueRouter(),
-    Layouts(),
-    Pages(),
-    Vue({
-      template: { transformAssetUrls }
-    }),
-    Vuetify({
+    vue(),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
       autoImport: true,
-      styles: {
-        configFile: 'src/styles/settings.scss',
-      },
     }),
-    Components(),
-    Fonts({
-      google: {
-        families: [{
-          name: 'Roboto',
-          styles: 'wght@100;300;400;500;700;900',
-        }],
-      },
-    }),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-      ],
-      eslintrc: {
-        enabled: true,
-      },
-      vueTemplate: true,
-    }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'My Vuetify App',
-        short_name: 'VuetifyApp',
-        description: 'My Vuetify App Description',
-        theme_color: '#4DBA87',
-        background_color: '#ffffff',
-        display: 'standalone',
-        orientation: 'portrait',
-        start_url: '/',
-        icons: [
-          {
-            src: './src/assets/Images/4.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: './src/assets/Images/4.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          }
-        ],
-        screenshots: [
-          {
-            src: './src/assets/Images/4.png',
-            sizes: '640x480',
-            type: 'image/png',
-            form_factor: 'narrow'
-          },
-          {
-            src: './assets/Images/4.png',
-            sizes: '1280x720',
-            type: 'image/png',
-            form_factor: 'wide'
-          }
-        ],
-        prefer_related_applications: true // در صورت وجود برنامه بومی، پیشنهاد نصب آن داده می‌شود
-      },
-      workbox: {
-        cleanupOutdatedCaches: true, // پاک کردن کش‌های قدیمی
-        globPatterns: ['**/*.{js,css,html,png}'], // فایل‌هایی که باید از پیش کش شوند
-        globDirectory: 'dist', // دایرکتوری مربوط به فایل‌ها
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/api\.example\.com\/.*$/,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 7, // 1 week
-              },
-              networkTimeoutSeconds: 10 // انتظار ۱۰ ثانیه برای شبکه قبل از استفاده از کش
-            }
-          }
-        ],
-      },
-      // تنظیمات مربوط به به‌روزرسانی سرویس‌ورکر
-      onRegisterSW: ({ update }) => {
-        update(); // اجرای آپدیت به صورت دستی
-      },
-      onUpdateReady: () => {
-        const updateServiceWorker = confirm('New version available. Update now?');
-        if (updateServiceWorker) {
-          update();
-        }
-      },
-    })
+    VitePWA(VitePWAConfig),
+    
   ],
   define: { 'process.env': {} },
+  // resolve: {
+  //   alias: {
+  //     '@':            path.resolve(__dirname, './node_modules/'),
+  //     'Components':   path.resolve(__dirname, './src/components'),
+  //     'Views':        path.resolve(__dirname, './src/views'),
+  //     'Helpers':      path.resolve(__dirname, './src/helpers'),
+  //     'Router':       path.resolve(__dirname, './src/router'),
+  //   },
+  // },
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
     extensions: [
       '.js',
       '.json',
@@ -127,9 +80,6 @@ export default defineConfig({
       '.ts',
       '.tsx',
       '.vue',
-    ],
-  },
-  server: {
-    port: 3000,
+    ]
   },
 })
