fbdc6262ef6c36a0566b03cb48a4cc0dcc25669a
m-javad7 <m.javad7721@gmail.com>
2024-08-11 22:25:25 +0330

    .

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,49 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from '@vuetify/vite-plugin'
-import { VitePWA } from 'vite-plugin-pwa'
-
-import path from 'path'
-
-
-const VitePWAConfig = {
-  mode: "development",
+import { fileURLToPath, URL } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import Fonts from 'unplugin-fonts/vite'
+import Layouts from 'vite-plugin-vue-layouts'
+import Vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
+import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import Pages from 'vite-plugin-pages'
+import { VitePWA } from 'vite-plugin-pwa' // اضافه کردن پلاگین PWA
+export default defineConfig({
+  plugins: [
+    VueRouter(),
+    Layouts(),
+    Pages(),
+    Vue({
+      template: { transformAssetUrls }
+    }),
+    Vuetify({
+      autoImport: true,
+      styles: {
+        configFile: 'src/styles/settings.scss',
+      },
+    }),
+    Components(),
+    Fonts({
+      google: {
+        families: [{
+          name: 'Roboto',
+          styles: 'wght@100;300;400;500;700;900',
+        }],
+      },
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+      ],
+      eslintrc: {
+        enabled: true,
+      },
+      vueTemplate: true,
+    }),
+    VitePWA({
+      mode: "development",
   base: "./",
   srcDir: "src",
   filename: "sw.ts",
@@ -46,32 +82,14 @@ const VitePWAConfig = {
       },
     ],
   },
-};
-
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  plugins: [
-    vue(),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
-    VitePWA(VitePWAConfig),
-    
+    })
   ],
+  
   define: { 'process.env': {} },
-  // resolve: {
-  //   alias: {
-  //     '@':            path.resolve(__dirname, './node_modules/'),
-  //     'Components':   path.resolve(__dirname, './src/components'),
-  //     'Views':        path.resolve(__dirname, './src/views'),
-  //     'Helpers':      path.resolve(__dirname, './src/helpers'),
-  //     'Router':       path.resolve(__dirname, './src/router'),
-  //   },
-  // },
   resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
     extensions: [
       '.js',
       '.json',
@@ -80,6 +98,9 @@ export default defineConfig({
       '.ts',
       '.tsx',
       '.vue',
-    ]
+    ],
+  },
+  server: {
+    port: 3000,
   },
 })
